15bf820843c59e677ad76f42cf7da74ba7655160
Mattias <mattiasrademar@gmail.com>
2018-12-17 09:56:43 +0100

    now when user wants to edit photo the old photo gets removed from the preview.

--- assets/scripts/post.js
+++ assets/scripts/post.js
@@ -1,9 +1,15 @@
 const fileUpload = document.querySelector('.file-upload')
 const description = document.querySelector('.description')
 const editPhotoURI = `/app/posts/update.php${window.location.search}`
+const previewImage = document.querySelector('.preview-image')
+
 
 const previewUploadedFiles = (files) => {
-	const previewImage = document.querySelector('.preview-image')
+	// Remove the images from preview if there is any.
+	// We do this so when the user wants to choose another image they dont stack.
+	if(previewImage.hasChildNodes) {
+		previewImage.removeChild(previewImage.firstChild)
+	}
 	for (let i = 0; i < files.length; i++) {
     let file = files[i];
 		let img = document.createElement('img')
@@ -27,14 +33,12 @@ const fetchPostForEdit = () => {
 		return res.json()
 	})
 	.then(json => {
-		console.log(json)
 		description.textContent = json.description
 		const previewImage = document.querySelector('.preview-image')
 		let file = json.image
 		let img = document.createElement('img')
 		img.classList.add('obj')
 		img.src = file
-		console.log(img)
 		previewImage.appendChild(img)
 	})
 }
