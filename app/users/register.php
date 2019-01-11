650afd8c22a778b017182270b3ed9b7223878ebd
Mattias <mattiasrademar@gmail.com>
2019-01-11 12:20:32 +0100

    making progress on the error handling for registration.

--- app/users/register.php
+++ app/users/register.php
@@ -12,8 +12,30 @@ if(isset($_POST['name'], $_POST['username'], $_POST['email'], $_POST['password']
 	if($password === $repeatPassword) {
 		$password = password_hash($password, PASSWORD_DEFAULT);
 	} else {
-		echo "Passwords do not match!";
+		$_SESSION['errors'] = 'Passwords do not match!';
+		redirect('/register.php');
+		exit;
 	}
+
+	$statement = $pdo->prepare('SELECT * FROM users WHERE username = :username OR email = :email');
+	$statement->bindParam(':username', $username, PDO::PARAM_STR);
+	$statement->bindParam(':email', $email, PDO::PARAM_STR);
+	$statement->execute();
+	
+	if(!$statement) {
+		die(var_dump($pdo->errorInfo()));
+	}
+
+	$user = $statement->fetch(PDO::FETCH_ASSOC);
+
+	if($user) {
+		$_SESSION['errors'] = 'A user with those credentials already exist, please try again';
+		redirect('/register.php');
+		exit;
+	}
+
+
+
 	$statement = $pdo->prepare('INSERT INTO users (name, username, email, password) 
 	VALUES (:name, :username, :email, :password)');
 	$statement->bindParam(':name', $name, PDO::PARAM_STR);
@@ -36,6 +58,6 @@ if(isset($_POST['name'], $_POST['username'], $_POST['email'], $_POST['password']
 	}
 
 	$_SESSION['user_authenticated'] = $user;
-
+	$_SESSION['errors'] = '';
 	redirect('/');
 }
--- register.php
+++ register.php
@@ -5,6 +5,9 @@ require __DIR__.'/views/header.php';
 ?>
 
 <div class="outer-container">
+<?php if (isset($_SESSION['errors'])): ?>
+<h5 class="error-message"> <?=$_SESSION['errors']; ?> </h5> 
+<?php endif; ?>
 	<form action="/app/users/register.php" method="POST">
 		<div class="register-input-container">
 			<input type="text" name="name" id="name" class="register-form-field" placeholder="Name">
@@ -20,6 +23,34 @@ require __DIR__.'/views/header.php';
 	</div>
 <!-- outer-container ends in footer. -->
 
+<script>
+	const name = document.querySelector('#name')
+	const username = document.querySelector('#username')
+	const email = document.querySelector('#email')
+
+	if(localStorage.getItem('userInfo')){
+		const user = JSON.parse(localStorage.getItem('userInfo'))
+		name.value = user.name || ''
+		username.value = user.username || ''
+		email.value = user.email || ''
+	}
+	
+	const form = document.querySelector('form')
+
+	form.addEventListener('submit', () => {
+		const userInfo = {
+		name: name.value,
+		username: username.value,
+		email: email.value
+		}
+
+		localStorage.setItem('userInfo', JSON.stringify(userInfo))
+
+	})
+</script>
+
 <?php 
 require __DIR__.'/views/footer.php';
 ?>
+
+
